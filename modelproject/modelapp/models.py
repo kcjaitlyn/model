8aa48a7b236b2704a3e2d47d3c0322c56585db89
kcjaitlyn <kcjaitlyn@naver.com>
2020-06-03 19:34:25 +0900

    model test

---
+++ modelproject/modelapp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    date = models.DateTimeField('date')
+    
+    # admon 글 제목 표시하기 위한 함수
+    def __str__(self):
+        return self.title
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    date = models.DateTimeField('date')
+    
+    # admon 글 제목 표시하기 위한 함수
+    def __str__(self):
+        return self.title
